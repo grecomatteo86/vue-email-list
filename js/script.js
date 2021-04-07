324eace6ea26b5284ccfd523f2f8d11f3dfa32b3
Matteo <grecomatteo86@gmail.com>
2021-04-07 15:45:02 +0200

    axios import and console.log of response

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+var app = new Vue (
+  {
+    el:"#app",
+    data:{
+      
+    },
+    mounted:function(){
 
+      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then(function (response) {
+        console.log(response.data.response);
+        // const result = response.data;
+      });
+    }
+  }
+)
