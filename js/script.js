cf967c298a1e54b64cdd49ae40c48d78ee995bb3
Matteo <grecomatteo86@gmail.com>
2021-04-07 16:52:07 +0200

    method 2 with arrow function

--- js/script.js
+++ js/script.js
@@ -7,21 +7,31 @@ var app = new Vue (
     },
     mounted:function(){
 
-      var self = this;
+      // METHOD 1 WITH SELF
+
+      // var self = this;
+      //
+      // for ( i = 0; i < 10; i++) {
+      //   axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      //   .then(function (response) {
+      //     self.mailArray.push(response.data.response);
+      //     console.log(self.mailArray);
+      //   });
+      // }
+
+
+      // METHOD 2 WITH ARROW FUNCTION
 
       for ( i = 0; i < 10; i++) {
-
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(function (response) {
+        .then ( (response) => {
 
-          self.mailArray.push(response.data.response);
-          console.log(self.mailArray);
+          this.mailArray.push(response.data.response);
+          console.log(this.mailArray);
 
-          
         });
 
       }
-
 
 
     }
