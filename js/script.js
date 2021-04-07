cdb8bcd434c99b0287672c7406c5090ec9a148d0
Matteo <grecomatteo86@gmail.com>
2021-04-07 15:50:40 +0200

    declaration var self

--- js/script.js
+++ js/script.js
@@ -2,14 +2,18 @@ var app = new Vue (
   {
     el:"#app",
     data:{
-      
+      mail:''
     },
     mounted:function(){
 
+      var self = this;
+
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then(function (response) {
-        console.log(response.data.response);
         // const result = response.data;
+        console.log(response.data.response);
+        self.mail = response.data.response;
+
       });
     }
   }
