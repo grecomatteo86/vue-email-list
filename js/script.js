be4daefd2e8a445c1aebf41d7306fc316d9afc8a
Matteo <grecomatteo86@gmail.com>
2021-08-03 22:45:44 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -6,34 +6,13 @@ var app = new Vue (
       mailArray:[]
     },
     mounted:function(){
-
-      // METHOD 1 WITH SELF
-
-      // var self = this;
-      //
-      // for ( i = 0; i < 10; i++) {
-      //   axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      //   .then(function (response) {
-      //     self.mailArray.push(response.data.response);
-      //     console.log(self.mailArray);
-      //   });
-      // }
-
-
-      // METHOD 2 WITH ARROW FUNCTION
-
       for ( i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then ( (response) => {
-
           this.mailArray.push(response.data.response);
           console.log(this.mailArray);
-
         });
-
       }
-
-
     }
   }
 )
